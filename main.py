a684afdd06883280592fb8a2de74b836c7ebe9f7
William Spanfelner <102249249+WilliamSpanfelner@users.noreply.github.com>
2022-06-16 10:57:58 +0300

    Remove request module as not required
     and amend form instantiation to reflect.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
